0426cacc5d6f92ea541bacc0444d0106dafee23c
ZV2J <zv2jpro@gmail.com>
2018-11-25 17:16:58 +0100

    0

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <SFML/System.hpp>
+
+/*
+    g++ -std=c++14 src/*.cpp -o bin/prog -I include -L lib -lsfml-graphics -lsfml-window -lsfml-system
+*/
+
+int main()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
